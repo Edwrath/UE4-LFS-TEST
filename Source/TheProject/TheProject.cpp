2b514a01c02db748d9310cf42d40a5c942b64c67
Steffen Eitelmann <steffen.eitelmann@unreadable.eu>
2016-05-21 11:55:56 +0200

    Cool

---
+++ Source/TheProject/TheProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheProject, "TheProject" );
